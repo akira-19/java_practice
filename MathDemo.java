3bab9cd2e4613bc93ed81f26826a9e9155511b99
Akira <38364091+akira-19@users.noreply.github.com>
2025-05-28 12:28:29 +0000

    update

---
+++ MathDemo.java
@@ -0,0 +1,7 @@
+class MathDemo{
+    public static void main(String args[]) {
+        System.out.println("Max of -8 and -4 is " + Math.max(-8,-4));
+        System.out.println("Min of -8 and -4 is " + Math.min(-8,-4));
+        System.out.println("PI = " + Math.PI);
+    }
+}
---
+++ SquareRoot.java
@@ -0,0 +1,5 @@
+class SquareRoot {
+    public static void main(String[] args) {
+        System.out.println("Square root of 25.38 is " + Math.sqrt(25.38));
+    }
+}
